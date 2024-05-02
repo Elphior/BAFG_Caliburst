1c47f4992f62a85ec0de663497c1e7e4a80c5c72
Sungmin Moon <elphior0@gmail.com>
2024-05-01 19:36:44 -0700

    Tested character select, functional instance. DO NOT USE BASEGAMEINSTANCE, only use FighterGI.

---
+++ Source/BAFG_Caliburst/BaseGameInstance.h
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "BaseGameInstance.generated.h"
+
+UCLASS()
+class BAFG_CALIBURST_API UBaseGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
